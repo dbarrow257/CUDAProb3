ca8c0376572f532fd94980346f57ed7bd45242cf
dbarrow257 <danbarrow257@gmail.com>
2021-09-23 14:58:10 +0100

    Moving code around to remove differences in github diff

--- physics.hpp
+++ physics.hpp
@@ -429,6 +429,87 @@ namespace cudaprob3{
                 }
             }
 	  
+	  template<typename FLOAT_T>
+	  HOSTDEVICEQUALIFIER
+          void getA(FLOAT_T L, FLOAT_T E, FLOAT_T rho, FLOAT_T d_dmMatVac[][3], FLOAT_T d_dmMatMat[][3],NeutrinoType type, FLOAT_T phase_offset, math::ComplexNumber<FLOAT_T> A[3][3]) {
+
+	    /* (1/2)*(1/(h_bar*c)) in units of GeV/(eV^2-km) */
+	    const FLOAT_T LoEfac = 2.534;
+	    
+	    math::ComplexNumber<FLOAT_T> product[3][3][3];
+
+	    if (phase_offset == 0.0) {
+	      FLOAT_T L = NAN;
+	      get_product(L, E, rho, d_dmMatVac, d_dmMatMat, type, product);
+	    }
+
+	    math::ComplexNumber<FLOAT_T> X[3][3];
+
+	    /* Make the sum with the exponential factor in Eq. (11) */
+	    //memset(X, 0, 3*3*sizeof(math::ComplexNumber<FLOAT_T>));
+	    UNROLLQUALIFIER
+	      for (int i=0; i<3; i++) {
+		UNROLLQUALIFIER
+		  for (int j=0; j<3; j++) {
+		    X[i][j].re = 0;
+		    X[i][j].im = 0;
+		  }
+	      }
+	    
+	    
+	    UNROLLQUALIFIER
+	      for (int k=0; k<3; k++) {
+		const FLOAT_T arg = [&](){
+		  if( k == 2)
+		    return -LoEfac * d_dmMatVac[k][0] * L/E + phase_offset;
+		  else
+		    return -LoEfac * d_dmMatVac[k][0] * L/E;
+		}();
+		
+		
+#ifdef __CUDACC__
+		FLOAT_T c,s;
+		sincos(arg, &s, &c);
+#else
+		const FLOAT_T s = sin(arg);
+		const FLOAT_T c = cos(arg);
+#endif
+		UNROLLQUALIFIER
+		  for (int i=0; i<3; i++) {
+		    UNROLLQUALIFIER
+		      for (int j=0; j<3; j++) {
+			X[i][j].re += c*product[i][j][k].re - s*product[i][j][k].im;
+			X[i][j].im += c*product[i][j][k].im + s*product[i][j][k].re;
+		      }
+		  }
+	      }
+	    
+	    clear_complex_matrix(A);
+
+	    //DB Think AXFAC(incoming nu flav, outgoing nu flav, row, column, part)
+
+	    UNROLLQUALIFIER
+	      for (int n=0; n<3; n++) {
+		UNROLLQUALIFIER
+		  for (int m=0; m<3; m++) {
+		    UNROLLQUALIFIER
+		      for (int i=0; i<3; i++) {
+			UNROLLQUALIFIER
+			  for (int j=0; j<3; j++) {
+			    // use precomputed factors
+			    A[n][m].re +=
+			      AXFAC(n,m,i,j,0) * X[i][j].re +
+			      AXFAC(n,m,i,j,1) * X[i][j].im;
+			    A[n][m].im +=
+			      AXFAC(n,m,i,j,2) * X[i][j].im +
+			      AXFAC(n,m,i,j,3) * X[i][j].re;
+			  }
+		      }
+		  }
+	      }
+
+	  }
+
 	  /***********************************************************************
   getArg
   
@@ -513,86 +594,6 @@ namespace cudaprob3{
 	    
           }
 
-	  template<typename FLOAT_T>
-	  HOSTDEVICEQUALIFIER
-          void getA(FLOAT_T L, FLOAT_T E, FLOAT_T rho, FLOAT_T d_dmMatVac[][3], FLOAT_T d_dmMatMat[][3],NeutrinoType type, FLOAT_T phase_offset, FLOAT_T Arg[3], math::ComplexNumber<FLOAT_T> A[3][3]) {
-
-	    /* (1/2)*(1/(h_bar*c)) in units of GeV/(eV^2-km) */
-	    const FLOAT_T LoEfac = 2.534;
-	    
-	    math::ComplexNumber<FLOAT_T> product[3][3][3];
-
-	    if (phase_offset == 0.0) {
-	      FLOAT_T L = NAN;
-	      get_product(L, E, rho, d_dmMatVac, d_dmMatMat, type, product);
-	    }
-
-	    math::ComplexNumber<FLOAT_T> X[3][3];
-
-	    /* Make the sum with the exponential factor in Eq. (11) */
-	    //memset(X, 0, 3*3*sizeof(math::ComplexNumber<FLOAT_T>));
-	    UNROLLQUALIFIER
-	      for (int i=0; i<3; i++) {
-		UNROLLQUALIFIER
-		  for (int j=0; j<3; j++) {
-		    X[i][j].re = 0;
-		    X[i][j].im = 0;
-		  }
-	      }
-	    
-	    
-	    UNROLLQUALIFIER
-	      for (int k=0; k<3; k++) {
-		const FLOAT_T arg = [&](){
-		  if( k == 2)
-		    return -LoEfac * d_dmMatVac[k][0] * L/E + phase_offset;
-		  else
-		    return -LoEfac * d_dmMatVac[k][0] * L/E;
-		}();
-		
-		
-#ifdef __CUDACC__
-		FLOAT_T c,s;
-		sincos(arg, &s, &c);
-#else
-		const FLOAT_T s = sin(arg);
-		const FLOAT_T c = cos(arg);
-#endif
-		UNROLLQUALIFIER
-		  for (int i=0; i<3; i++) {
-		    UNROLLQUALIFIER
-		      for (int j=0; j<3; j++) {
-			X[i][j].re += c*product[i][j][k].re - s*product[i][j][k].im;
-			X[i][j].im += c*product[i][j][k].im + s*product[i][j][k].re;
-		      }
-		  }
-	      }
-	    
-	    clear_complex_matrix(A);
-
-	    //DB Think AXFAC(incoming nu flav, outgoing nu flav, row, column, part)
-
-	    UNROLLQUALIFIER
-	      for (int n=0; n<3; n++) {
-		UNROLLQUALIFIER
-		  for (int m=0; m<3; m++) {
-		    UNROLLQUALIFIER
-		      for (int i=0; i<3; i++) {
-			UNROLLQUALIFIER
-			  for (int j=0; j<3; j++) {
-			    // use precomputed factors
-			    A[n][m].re +=
-			      AXFAC(n,m,i,j,0) * X[i][j].re +
-			      AXFAC(n,m,i,j,1) * X[i][j].im;
-			    A[n][m].im +=
-			      AXFAC(n,m,i,j,2) * X[i][j].im +
-			      AXFAC(n,m,i,j,3) * X[i][j].re;
-			  }
-		      }
-		  }
-	      }
-
-	  }
 
 	  //##########################################################
 	  /*
@@ -623,7 +624,7 @@ namespace cudaprob3{
 	    getMfast(Enu, rho, nutype, d_dmMatMat, d_dmMatVac);
 	    
 	    getArg(Len, Enu, d_dmMatVac, Arg, phase_offset);
-	    getA(Len, Enu, rho, d_dmMatVac, d_dmMatMat, nutype, phase_offset, Arg, Aout);
+	    getA(Len, Enu, rho, d_dmMatVac, d_dmMatMat, nutype, phase_offset, Aout);
 	  }
 
             /*
