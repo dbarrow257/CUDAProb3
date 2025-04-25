a1294b1cdabbda811c478075ea5dea896f7e04f2
M-Ismerio <129776710+M-Ismerio@users.noreply.github.com>
2025-04-25 16:37:33 -0300

    Update constants.hpp to run with GPU
    Co-authored-by: Ben Jargowsky <ben@benscomputer.page>

--- constants.hpp
+++ constants.hpp
@@ -22,6 +22,7 @@ along with CUDAProb3++.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace cudaprob3{
 
+    HOSTDEVICEQUALIFIER
     FLOAT_T EarthRadius = 6371.0;
     
     template<typename FLOAT_T>
